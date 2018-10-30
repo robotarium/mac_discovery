6ff1bba6ac1bfd927e70ac4de55c5e5e1a2e65d4
robotarium-workstation <you@example.com>
2018-10-30 19:10:29 -0400

    don't watchtower from here

--- docker/docker_watch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-	--restart always \
-	--name watchtower_mac \
-	-v /var/run/docker.sock:/var/run/docker.sock \
-	v2tec/watchtower:armhf-latest -i 60 --debug mac_discovery
