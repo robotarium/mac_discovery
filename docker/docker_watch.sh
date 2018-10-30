54b7bbd587edcc238346c4e5d509fb8593593590
robotarium-workstation <you@example.com>
2018-10-30 17:34:08 -0400

    yupdates

--- docker/docker_watch.sh
+++ docker/docker_watch.sh
@@ -3,4 +3,4 @@
 docker run -d \
 	--name watchtower_mac \
 	-v /var/run/docker.sock:/var/run/docker.sock \
-	v2tec/watchtower -i 60 --debug mac_discovery
+	v2tec/watchtower:armhf-latest -i 60 --debug mac_discovery
